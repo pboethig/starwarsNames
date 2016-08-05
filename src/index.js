21c03526ae13fff260f9bea81bad3ff575be312f
pboethig <pboethig@gmail.com>
2016-08-05 18:44:54 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+var uniqueRandomArray = require('unique-random-array');
+
+var starWarsNames = require('./starwars-names.json');
+
+module.exports = 
+{
+    all: starWarsNames,
+    random:uniqueRandomArray(starWarsNames)
+};
